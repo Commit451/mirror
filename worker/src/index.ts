57453315b8a672ec6eb66a6be8f1496b9e1865dc
theJawnnybot <iamjawnnybot@gmail.com>
2026-02-20 16:03:27 -0600

    Use Commit451 favicon

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -302,6 +302,7 @@ function generateHTML(
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="/favicon.png">
   <title>Index of ${safePath}</title>
   <style>
     :root {
